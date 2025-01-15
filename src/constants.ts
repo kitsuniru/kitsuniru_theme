5954985f30d50ca27c31f0c3922ed6b604f67a64
kitsuniru <vps.sayder@gmail.com>
2025-01-15 03:24:09 +0300

    refactor: export cui identifier to `constants.ts`

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
 export const extensionFullName = 'kitsuniru.kitsuniru-theme';
 export const extensionDisplayName = "Kitsuniru Theme";
 export const lastVersionKey = 'kitsuniru_last_version';
+
+export const customUiStyleExtensionId = 'subframe7536.custom-ui-style';
--- src/styling_utils.ts
+++ src/styling_utils.ts
@@ -1,7 +1,9 @@
 
 import * as vscode from 'vscode';
+import { customUiStyleExtensionId } from './constants';
+
 export const validateCustomUiStyle = () => {
-    const customUiStyle = vscode.extensions.getExtension('subframe7536.custom-ui-style');
+    const customUiStyle = vscode.extensions.getExtension(customUiStyleExtensionId);
     if (customUiStyle == undefined) {
         vscode.window.showErrorMessage('Unable to find `Custom UI Style` extension.\nPlease, install it.');
         return;
